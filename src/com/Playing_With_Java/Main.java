0d3d5d20559f85faffea69827b8c5c4391fd0e34
James Wood <james_s_wood@outlook.com>
2021-01-29 11:19:59 -0500

    Initial commit

---
+++ src/com/Playing_With_Java/Main.java
@@ -0,0 +1,10 @@
+package com.Playing_With_Java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Playing with Java");
+        System.out.println("This is so cool!");
+    }
+}
